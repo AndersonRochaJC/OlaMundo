96046109321061513e3c2d78eb39919ad381458c
AndersonRochaJC <jmcreggae@gmail.com>
2021-08-23 21:47:06 -0300

    OlaMundo

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    class Program
+    {        static void Main(string[] args)
+       {     Console.BackgroundColor = ConsoleColor.Green;
+             Console.WriteLine("Ola Mundo!!!");  
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Shalom!!!!");
+             Console.BackgroundColor = ConsoleColor.Red;
+             Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Eu me chamo Anderson Rocha!");
+              Console.ResetColor();
+
+
+        } 
+    }
+}
